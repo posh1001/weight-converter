e79b5fc633c192b2d3fdaf2cb73f442141f2c9b8
unknown <orifepaul30@gmail.com>
2025-11-07 18:37:46 +0100

    for loop

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-command = ""
-started = False
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        if started:
-            print("Car is already started!")
-        else:
-            started = True
-            print("Car Started...")
-    elif command == "stop":
-        if not started:
-            print("Car is already stopped")
-        else:
-            started = False
-            print("Car Stopped...")
-    elif command == "help":
-        print("""
-start - Start the car
-stop - Stop the car
-quit - to quit
-        """)
-    elif command == "quit":
-        break
-    else:
-        print("Sorry, I don't understand that!")
+numbers = [2, 2,  2, 7]
+for x_count in numbers:
+    output = ""
+    for count in range(x_count):
+        output += 'x'
+    print(output)
