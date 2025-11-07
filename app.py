a726e7abc87fbd73f7b829358346af606fb6961b
unknown <orifepaul30@gmail.com>
2025-11-07 08:06:23 +0100

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-weight = int(input("weight: "))
-unit = input('(L)bs or (k)g: ')
-if unit.upper() == 'L':
-    converted = weight * 0.45
-    print(f"You are {converted} kilos")
+secret_number = 12
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("Correct!")
+        break
 else:
-    converted = weight / 0.45
-    print(f"You are {converted} pounds")
+    print("Wrong!")
