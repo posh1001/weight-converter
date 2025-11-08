b23b8e8d7e3113aa3077688c02358e3ed256ea8d
unknown <orifepaul30@gmail.com>
2025-11-08 20:33:57 +0100

    dictionary

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-phone = input("phone: ")
-digit_mapping = {
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four",
+message = input(">")
+words = message.split(" ")
+emojis = {
+    ":)": "😊",
+    ":(": "😔"
 }
 
 output = ""
-for ch in phone:
-    output+=digit_mapping.get(ch, "!")  +  ""
+for word in words:
+    output += emojis.get(word, word) + " "
 print(output)
